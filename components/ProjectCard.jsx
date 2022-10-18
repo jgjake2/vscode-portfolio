7fe205ba7414488f1237006ae972d084712a5aaf
jgjake2 <jgjake22@gmail.com>
2022-10-18 17:50:18 -0500

    Update Gyro HD 3 Editor thumbnail

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -7,7 +7,7 @@ const ProjectCard = ({ project }) => {
       var gyroHDEditor = document.querySelector('img[src*="gyrohd3editor"]');
       if(gyroHDEditor){
           //gyroHDEditor.style.margin = '0px';
-          gyroHDEditor.style.padding = '1rem';
+          gyroHDEditor.style.padding = '0px';
       }
         
       var starWarsImage = document.querySelector('img[src*="SWUP_Logo"]');
